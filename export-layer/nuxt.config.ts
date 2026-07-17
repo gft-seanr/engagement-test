69859a796d0048e9e614124b4407c29d73735c03
Sean Rances <sean@giftaway.ph>
2026-07-17 14:54:13 +0800

    test

--- export-layer/nuxt.config.ts
+++ export-layer/nuxt.config.ts
@@ -1,20 +1,5 @@
-import { fileURLToPath } from 'node:url';
-
-// This dynamically grabs the absolute path of the parent directory
-// no matter where Nuxt caches it on your machine.
-const parentDir = fileURLToPath(new URL('..', import.meta.url));
-
 export default defineNuxtConfig({
-  components: [{ path: `${parentDir}/components`, pathPrefix: false, global: true }],
-  imports: {
-    dirs: [`${parentDir}/composables`, `${parentDir}/utils`, `${parentDir}/types`],
-  },
-  alias: {
-    '~': parentDir,
-    '@': parentDir,
-    '~~': parentDir,
-    '@@': parentDir,
-  },
-  // If you also want to share server/utils or server/api routes, uncomment this:
-  // serverDir: `${parentDir}/server`
+  // Tell Nuxt to look one folder up for all the code,
+  // automatically handling aliases (~/@), components, and the server folder.
+  srcDir: '..',
 });
